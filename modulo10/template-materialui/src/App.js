e45427afe3649574402d6d3bd014602a7ae1f37d
Anderson <andersonapm15@gmail.com>
2024-05-24 19:52:59 -0300

    CSS e JS

--- modulo10/template-materialui/src/App.js
+++ modulo10/template-materialui/src/App.js
@@ -1,11 +1,17 @@
-import Header from "./components/header";
+import React from "react";
+import Header from "./components/Header";
 import ListarTarefa from "./pages/tarefa/ListarTarefa";
 
 function App() {
+  const handleClick = () => {
+    alert('Botão clicado!');
+  };
+
   return (
     <div className="App">
       <Header />
       <ListarTarefa />
+      <button onClick={handleClick}>Clique-me</button>
     </div>
   );
 }
